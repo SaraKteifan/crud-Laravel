d8a4a7d750b5e085f4eed86f5398ace1a4e459e5
SaraKteifan <100211522+SaraKteifan@users.noreply.github.com>
2022-06-07 11:37:32 +0300

    One to Many Relationship

---
+++ oneToMany/app/Models/Children.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Children extends Model
+{
+    use HasFactory;
+    public function father(){
+        return $this->belongsTo(Father::class);
+    }
+}
